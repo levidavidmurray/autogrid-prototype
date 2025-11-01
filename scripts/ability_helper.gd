90cbf87db7a3601f71d0f7dfd71e1b3ec84f1d4f
Levi Murray <levi@levimurray.dev>
2025-10-31 18:04:30 -0700

    AbilityHelper target cell look up for DIRECTED abilities

--- scripts/ability_helper.gd
+++ scripts/ability_helper.gd
@@ -1,14 +1,38 @@
 class_name AbilityHelper
 
 static func get_possible_target_cells(ability: AbstractAbility, owner_cell: CellData) -> Array[CellData]:
-	var targets: Array[CellData] = []
+	match ability.cast_type:
+		AbstractAbility.ECastType.ADJACENT:
+			return _get_adjacent_target_cells(owner_cell)
+		AbstractAbility.ECastType.DIRECTED:
+			return _get_directed_target_cells(owner_cell)
 
-	if ability.cast_type == AbstractAbility.ECastType.ADJACENT:
-		targets = owner_cell.get_neighbor_cells()
-
-	return targets
+	return []
 
 
 static func can_target_cell(ability: AbstractAbility, owner_cell: CellData, target_cell: CellData) -> bool:
 	var possible_cells = get_possible_target_cells(ability, owner_cell)
 	return target_cell in possible_cells
+
+
+static func _get_adjacent_target_cells(cell: CellData) -> Array[CellData]:
+	return cell.get_neighbor_cells()
+
+
+static func _get_directed_target_cells(cell: CellData) -> Array[CellData]:
+	var neighbors = cell.get_neighbor_cells()
+	var cells: Array[CellData] = neighbors.duplicate()
+
+	var cur_cell: CellData
+	for neighbor in neighbors:
+		cur_cell = neighbor
+		var dir = Grid.instance.get_direction_to_cell(cell, neighbor)
+		if cur_cell.occupant:
+			continue
+		while Grid.instance.is_valid_coord(cur_cell.coord + dir):
+			cur_cell = Grid.instance.grid_to_cell(cur_cell.coord + dir)
+			cells.append(cur_cell)
+			if cur_cell.occupant:
+				break
+
+	return cells
--- scripts/actions/push_action.gd
+++ scripts/actions/push_action.gd
@@ -2,7 +2,7 @@ class_name PushAction
 extends AbstractAction
 
 static func create(source: CellData, target: TileOccupant):
-    var push_dir = Grid.instance.get_direction_to_cell(source, target.cell)
+    var push_dir = Grid.instance.get_direction_to_cell(source, target.cell).clampi(-1, 1)
     var new_cell = Grid.instance.get_relative_cell(target.cell, push_dir)
 
     if new_cell and new_cell.occupant != null:
@@ -19,12 +19,13 @@ static func create(source: CellData, target: TileOccupant):
         target_pos = cur_pos + (Vector3(push_dir.x, 0.0, push_dir.y) * grid_square.cell_size)
 
     var tween = target.body.create_tween()
-    tween.set_trans(Tween.TRANS_QUAD)
+    tween.set_trans(Tween.TRANS_SINE)
     tween.set_ease(Tween.EASE_OUT)
     tween.tween_property(target.body, "global_position", target_pos, push_time)
 
     if new_cell == null:
-        tween.tween_property(target.body, "global_position", target.cell.position, push_time * 0.75)
+        # Pushing off grid. Tween back to current cell
+        tween.tween_property(target.body, "global_position", target.cell.position, push_time)
 
     await tween.finished
     await MoveAction.create(target, push_dir)
